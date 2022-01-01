698df6150aaf43bc17207ff0db28a9b641e3a726
Thomas Lobjoie <thomaslobjoie@users.noreply.github.com>
2022-01-01 16:50:47 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    timer = randint(5, 20)
+})
+let timer = 0
+timer = -1
+basic.forever(function () {
+    if (timer > 0) {
+        basic.pause(1000)
+        basic.showIcon(IconNames.SmallDiamond)
+        timer += -1
+    }
+    if (timer == 0) {
+        basic.showIcon(IconNames.Skull)
+        music.playMelody("B G A F C G C E ", 120)
+        timer = -1
+    }
+    if (timer == -1) {
+        basic.showIcon(IconNames.Yes)
+    }
+})
